e586ee37858ef47ad1de495a3d447d3a12667084
Ar4ySmoke <developerar4y@gmail.com>
2023-04-27 16:40:08 +0300

    three_comit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ $("#btn-popup").click(()=> {
 $("#btn-close").click(()=> {
     $(".popup").toggleClass("hidden")
 })
+
+$("#btn-popup_01").click(()=> {
+        $(".popup").toggleClass("hidden")
+}) 
+$("#btn-close_01").click(()=> {
+    $(".popup").toggleClass("hidden")
+})
+
+$("#btn-popup_anketa").click(()=> {
+        $(".popup_anketa").toggleClass("hidden")
+}) 
+$("#btn-close_anketa").click(()=> {
+    $(".popup_anketa").toggleClass("hidden")
+})
